19d4ccc871cf7799b66e49d5665a36eb1199e52d
thenghiabk <thenghiabk@gmail.com>
2025-02-07 16:29:29 +0700

    update ArrayListADT

--- src/L01_ArrayListADT/ArrayListADT.java
+++ src/L01_ArrayListADT/ArrayListADT.java
@@ -1,28 +1,53 @@
 package L01_ArrayListADT;
 
+import java.util.Arrays;
+
 public class ArrayListADT implements AbstractList {
 
     // attributes
     private int[] elements; // stored in the stack memory
+    int nextIndex;
 
     // constructor
     public ArrayListADT(){
         elements = new int[5];
+        nextIndex = 0;
     }
 
     // methods
 
     @Override
     public boolean add ( int element ) {
+        if (nextIndex == elements.length) {
+            elements = Arrays.copyOf( elements, elements.length*2 );
+        }
 
-
+        elements[nextIndex] = element;
+        nextIndex++;
 
         return true;
     }
 
     @Override
     public boolean add ( int index, int element ) {
-        return false;
+        if (index < 0 || index >= nextIndex ){
+            throw new IndexOutOfBoundsException("Index out of bound.");
+        }
+
+        if (nextIndex == elements.length) {
+            elements = Arrays.copyOf( elements, elements.length*2 );
+        }
+
+        // shifts remaining elements to the right
+        for (int i = nextIndex; i >= index; i--){
+            elements[ i ] = elements[ i-1 ];
+        }
+
+        // insert new element
+        elements[index] = element;
+        nextIndex++;
+
+        return true;
     }
 
     @Override
@@ -58,5 +83,19 @@ public class ArrayListADT implements AbstractList {
     @Override
     public boolean isEmpty () {
         return false;
+    }
+}
+
+class ArrayListADTRunner{
+    public static void main ( String[] args ) {
+        ArrayListADT myArrayList = new ArrayListADT();
+        myArrayList.add( 10 );
+        myArrayList.add( 20 );
+        myArrayList.add( 30 );
+        myArrayList.add( 40 );
+        myArrayList.add( 50 );
+
+        myArrayList.add( 2, 60 );
+
     }
 }
