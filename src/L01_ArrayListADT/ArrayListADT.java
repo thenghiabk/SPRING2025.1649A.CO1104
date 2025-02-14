bf0e08652bf851a8c842bf42f57bd01e4553c055
thenghiabk <thenghiabk@gmail.com>
2025-02-14 15:20:55 +0700

    update ArrayListADT

--- src/L01_ArrayListADT/ArrayListADT.java
+++ src/L01_ArrayListADT/ArrayListADT.java
@@ -5,19 +5,20 @@ import java.util.Arrays;
 public class ArrayListADT<E> implements AbstractList<E> {
 
     // attributes
+    private static final int DEFAULT_CAPACITY = 5;
     private E[] elements; // stored in the stack memory
     int nextIndex;
 
     // constructor
     public ArrayListADT(){
-        elements = (E[]) new Object[5];
+        elements = (E[]) new Object[DEFAULT_CAPACITY];
         nextIndex = 0;
     }
 
     // methods
 
     @Override
-    public boolean add ( int element ) {
+    public boolean add ( E element ) {
         if (nextIndex == elements.length) {
             elements = Arrays.copyOf( elements, elements.length*2 );
         }
@@ -29,7 +30,7 @@ public class ArrayListADT<E> implements AbstractList<E> {
     }
 
     @Override
-    public boolean add ( int index, int element ) {
+    public boolean add ( int index, E element ) {
         if (index < 0 || index >= nextIndex ){
             throw new IndexOutOfBoundsException("Index out of bound.");
         }
@@ -51,7 +52,7 @@ public class ArrayListADT<E> implements AbstractList<E> {
     }
 
     @Override
-    public int get ( int index ) {
+    public E get ( int index ) {
         if (index < 0 || index >= nextIndex ){
             throw new IndexOutOfBoundsException("Index out of bound.");
         }
@@ -60,30 +61,30 @@ public class ArrayListADT<E> implements AbstractList<E> {
     }
 
     @Override
-    public int set ( int index, int element ) {
+    public E set ( int index, E element ) {
         if (index < 0 || index >= nextIndex ){
             throw new IndexOutOfBoundsException("Index out of bound.");
         }
 
-        int oldElement = elements[index];
+        E oldElement = elements[index];
 
         elements[index] = element;
         return oldElement;
     }
 
     @Override
-    public int remove ( int index ) {
+    public E remove ( int index ) {
         if (index < 0 || index >= nextIndex ){
             throw new IndexOutOfBoundsException("Index out of bound.");
         }
 
-        int oldElement = elements[index];
+        E oldElement = elements[index];
 
         // shift remain elements to the left
         for(int i = index; i < nextIndex - 1; i++){
             elements[i] = elements[ i+1 ];
         }
-        elements[nextIndex-1] = 0;
+        elements[nextIndex-1] = null;
         nextIndex--;
 
         if (nextIndex < elements.length / 3){
@@ -99,7 +100,7 @@ public class ArrayListADT<E> implements AbstractList<E> {
     }
 
     @Override
-    public int indexOf ( int element ) {
+    public int indexOf ( E element ) {
         for (int i = 0; i < nextIndex; i++){
             if (elements[i] == element){
                 return i;
@@ -110,8 +111,8 @@ public class ArrayListADT<E> implements AbstractList<E> {
     }
 
     @Override
-    public boolean contains ( int element ) {
-        for (int item : elements){
+    public boolean contains ( E element ) {
+        for (var item : elements){
             if (item == element){
                 return true;
             }
@@ -140,16 +141,23 @@ class ArrayListADTRunner{
 
         myArrayList.add( 2, 60 );
 
-        System.out.println(myArrayList.get(2));
+        System.out.println(myArrayList.get(2)); // 60
 
         myArrayList.set(2, 80);
 
-        System.out.println(myArrayList.get(2));
+        System.out.println(myArrayList.get(2)); // 80
 
         myArrayList.remove(0);
         myArrayList.remove(0);
         myArrayList.remove(0);
         myArrayList.remove(0);
+
+        ArrayListADT<String> myStringArrayList = new ArrayListADT<>();
+        myStringArrayList.add("Hello");
+        myStringArrayList.add("World");
+
+        System.out.println(myStringArrayList.get(0)); // Hello
+        System.out.println(myStringArrayList.get(1)); // World
 
     }
 }
