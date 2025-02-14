06a7c0edccbf00d4ba44ee17a1c54a7c38d41d33
thenghiabk <thenghiabk@gmail.com>
2025-02-14 17:09:35 +0700

    update LinkedListADT

--- src/L01_LinkedListADT/LinkedListADT.java
+++ src/L01_LinkedListADT/LinkedListADT.java
@@ -1,14 +1,41 @@
 package L01_LinkedListADT;
 
 public class LinkedListADT<E> implements AbtractLinkedList<E> {
+    private Node head;
+
+    // constructor
+    public LinkedListADT(){
+        this.head = null;
+    }
 
     @Override
     public void addFirst ( E element ) {
+        Node newNode = new Node( element );
+
+        if (head == null){
+            head = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+
 
     }
 
     @Override
     public void addLast ( E element ) {
 
+    }
+
+    private class Node {
+        // data
+        private E element;
+        private Node next;
+
+        // constructor
+        public Node ( E element ) {
+            this.element = element;
+            this.next = null;
+        }
     }
 }
