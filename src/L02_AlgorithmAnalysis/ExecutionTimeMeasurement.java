77b604aee6c398311ab116704099a4cabb899488
thenghiabk <thenghiabk@gmail.com>
2025-02-28 14:49:06 +0700

    update L04_BigO

--- src/L02_AlgorithmAnalysis/ExecutionTimeMeasurement.java
+++ src/L02_AlgorithmAnalysis/ExecutionTimeMeasurement.java
@@ -1,23 +1,47 @@
 package L02_AlgorithmAnalysis;
 
+
+/**
+ * Why do we have to compare complexity?
+ *
+ * 1. Resources: CPU, memory and storage.
+ * 2. Scalability: Can the program handle large input?
+ * 3. Performance: How fast does the program run?
+ * 4. Code quality: Is the code readable, maintainable and efficient?
+ * 5. Problem solving: Can the program solve the problem correctly?
+ */
+
+/**
+ * This program compares the execution time of two methods to concatenate a string
+ * a specified number of times. The first method uses String, the second method
+ * uses StringBuilder.
+ */
+
+
+
 public class ExecutionTimeMeasurement {
     public static void main(String[] args) {
 
-        //System.out.println(repeat1( "A", 50000 ));
-        //System.out.println(repeat2( "A", 50000 ));
-
+        // Number of iterations for the repeat functions
         int numOfLoop = 50_000;
-        long startTime = System.currentTimeMillis(); // get timestamps
-        /* algorithm */
-         repeat1("A", numOfLoop);
-        //repeat2("A", numOfLoop);
-        long endTime = System.currentTimeMillis(); // get timestamps
 
-        long elapsedTime = endTime - startTime; // milliseconds
+        // Record start time
+        long startTime = System.currentTimeMillis();
 
+        // Execute repeat1 function
+        repeat1("A", numOfLoop);
+
+        // Record end time
+        long endTime = System.currentTimeMillis();
+
+        // Calculate elapsed time in milliseconds
+        long elapsedTime = endTime - startTime;
+
+        // Output the execution time
         System.out.println("Execution time: " + elapsedTime + " (ms).");
     }
 
+    // Concatenates the string 'c' 'n' times using String
     public static String repeat1(String c, int n) {
         String answer = "";
         for (int i = 0; i < n; i++) {
@@ -26,6 +50,7 @@ public class ExecutionTimeMeasurement {
         return answer;
     }
 
+    // Concatenates the string 'c' 'n' times using StringBuilder
     public static String repeat2(String c, int n) {
         StringBuilder answer = new StringBuilder();
         for (int i = 0; i < n; i++) {
